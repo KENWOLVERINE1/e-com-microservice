86fae00573e39e6bfec4bbb5d6533483e20f4a9f
KENWOLVERINE1 <kenwolverine1@gmail.com>
2025-06-23 20:21:10 +0530

    reinitializing

--- backend/product-service/Product.js
+++ backend/product-service/Product.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 
 const productScheme = new mongoose.Schema({
     Pid:{type:Number,required:true,unique:true},
-    Pname:{type:String,required:true,unique:true},
+    Pname:{type:String,required:true},
     Pdescription:String,
     Pprice:{type:Number,required:true},
     Pstock:{type:Number,required:true},
     Pcolor:{type:String,required:true},
     Psize:{type:String,required:true},
-    category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+    // category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+    category:{type:String,required:true},
     Pimg:{type:String, required:true}
 })
